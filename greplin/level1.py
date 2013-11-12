8684716dfec49c594f8581e7f7c592f827066c76
Peter Evans <evans.peter@gmail.com>
2013-11-12 09:04:51 +0000

    PEP8 fixes.

--- greplin/level1.py
+++ greplin/level1.py
@@ -1,45 +1,45 @@
  #!/usr/bin/env python
 
 def main():
-	"""
-	The Greplin Programming Challenge
+    """
+    The Greplin Programming Challenge
 
-	Level 1
+    Level 1
 
-	----------------------------------------
+    ----------------------------------------
 
-	Embedded in this block of text is the password for level 2.
-	The password is the longest substring that is the same in reverse.
+    Embedded in this block of text is the password for level 2.
+    The password is the longest substring that is the same in reverse.
 
-	As an example, if the input was "I like racecars that go fast"
-	the password would be "racecar".
-	"""
-	# Track the longest palindrome.
-	longest = ''
+    As an example, if the input was "I like racecars that go fast"
+    the password would be "racecar".
+    """
+    # Track the longest palindrome.
+    longest = ''
 
-	# Read the gettysburg text.
-	text = open('gettysburg.txt', 'r').read()
+    # Read the gettysburg text.
+    text = open('gettysburg.txt', 'r').read()
 
-	# Enumerate over each letter in the corpus.
-	for position, letter in enumerate(text):
-		# Track the current position, we will use this
-		# value to loop backwards with.
-		back = position
-		# While the back variable isn't 0.
-		while back != 0:
-			# Extract the substring.
-			substring = text[back:position + 1]
-			# If the substring is the same as itself in reverse
-			# and it's longer than the current longest palindrome,
-			# make it the new longest one.
-			if substring == substring[::-1] and len(substring) > len(longest):
-				longest = substring
-			# Decrement our back position.
-			back -= 1
+    # Enumerate over each letter in the corpus.
+    for position, letter in enumerate(text):
+        # Track the current position, we will use this
+        # value to loop backwards with.
+        back = position
+        # While the back variable isn't 0.
+        while back != 0:
+            # Extract the substring.
+            substring = text[back:position + 1]
+            # If the substring is the same as itself in reverse
+            # and it's longer than the current longest palindrome,
+            # make it the new longest one.
+            if substring == substring[::-1] and len(substring) > len(longest):
+                longest = substring
+            # Decrement our back position.
+            back -= 1
 
-	# And print the password to level 2.
-	print 'The password to level 2 is "%s".' % longest
+    # And print the password to level 2.
+    print 'The password to level 2 is "%s".' % longest
 
 
 if __name__ == '__main__':
-	main()
+    main()
